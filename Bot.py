0a5add74bead3e4067e2d3b5111effc0b70cab21
zzsxd <nikitacapkov06@gmail.com>
2024-01-26 23:37:44 +0800

    add little backend and frontend functions

--- Bot.py
+++ Bot.py
@@ -4,10 +4,13 @@
 #                     SBR                       #
 #################################################
 import telebot
-from Frontend import Bot_inline_btns
+from Frontend import Bot_inline_btns, Bot_text
+from Backend import User_data
 ############static variables#####################
 TG_api = '6473206457:AAGDDCArrBFIXTdFT1rGJqPVR1CjXqhugeM'
 admins = [818895144, 1897256227]
+#zzsxd - 1897256227
+#sbr - 818895144
 #################################################
 
 bot = telebot.TeleBot(TG_api)
@@ -18,9 +21,24 @@ def start(message):
     buttons = Bot_inline_btns()
     command = message.text.replace('/', '')
     if command == 'start':
-        bot.send_message(message.chat.id, 'Hello! Choose language!', reply_markup=buttons.start_btns())
+        user_data.init(message.chat.id)
+        bot.send_message(message.chat.id, 'Hello! Choose language!\nПривет! Выберите язык!', reply_markup=buttons.start_btns())
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+
+    buttons = Bot_inline_btns()
+    if call.data == 'rus':
+        pass
+    elif call.data == 'eng':
+        pass
 
 
 
+
+
+phrase = Bot_text()
+user_data = User_data()
 
 bot.polling(none_stop=True)
--- Frontend.py
+++ Frontend.py
@@ -5,12 +5,27 @@
 #################################################
 from telebot import types
 
+
 class Bot_inline_btns:
     def __init__(self):
         super(Bot_inline_btns, self).__init__()
         self.__markup = types.InlineKeyboardMarkup(row_width=2)
+
     def start_btns(self):
         rusbtn = types.InlineKeyboardButton('🇷🇺', callback_data='rus')
         engbtn = types.InlineKeyboardButton('🇺🇸', callback_data='eng')
         self.__markup.add(rusbtn, engbtn)
         return self.__markup
+
+
+class Bot_text:
+    def __init__(self):
+        super(Bot_text, self).__init__()
+        self.__rus = {'info': ['негор']}
+        self.__eng = {'info': ['BBC']}
+
+    def get_rus(self, type_):
+        return self.__rus[type_]
+
+    def get_eng(self, type_):
+        return self.__eng[type_]
